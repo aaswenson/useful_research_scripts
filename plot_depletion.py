12ddaffe009c6917a2fe53b5eabe4477971ec58c
Alexander Swenson <aaswenson@wisc.edu>
2018-05-01 13:10:29 -0500

    depletion plotter/parser

--- plot_depletion.py
+++ plot_depletion.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 
 def get_lines(filename='outp'):
     
@@ -43,8 +44,28 @@ def parse_table_210(lines):
 
     return time, BU
 
+def plot_results(keff, rel_err, ind_vars):
+
+
+    fig, axes = plt.subplots() # Size of 12 inches X 8 inches.
+    axes.set_title("Homogeneous Depletion (300 K)")
+    axes.set_xlabel("Time [days]", fontsize=12)
+    axes.set_ylabel("keff [-]", fontsize=12)
+
+    keff_list=[]
+    for idx, keff in enumerate(keff):
+        err = rel_err[idx]
+        ind = ind_vars[idx]
+
+        axes.errorbar(ind, keff, yerr=err, fmt='s', markersize=2,
+                      color='blue', capsize=4)
+
+    fig.savefig('depletion_results.eps', format='eps', dpi=2000)
+    
+
 
 lines = get_lines()
 keff, res_err = parse_keff(lines)
 time, BU = parse_table_210(lines)
-print(time)
+
+plot_results(keff, res_err, time)
